ddcc59da9321acc868b5d64700712fb43be0dde8
ichiko <ichigo_sky@hotmail.com>
2015-02-03 19:24:06 +0900

    Add compile macro USE_GOOGLE_ANALYTOCS

--- DotKioku/GoogleAnalyticsManager.swift
+++ DotKioku/GoogleAnalyticsManager.swift
@@ -8,6 +8,7 @@
 
 class GoogleAnalyticsManager {
     class func configure() {
+#if USE_GOOGLE_ANALYTICS
         let gai = GAI.sharedInstance()
         gai.trackUncaughtExceptions = true
         gai.dispatchInterval = 20.0
@@ -17,20 +18,27 @@ class GoogleAnalyticsManager {
         gai.dryRun = true
         gai.logger.logLevel = GAILogLevel.Verbose
 #endif
+#endif
     }
 
     class func setAppVersion(version:String) {
+#if USE_GOOGLE_ANALYTICS
         let tracker = GAI.sharedInstance().defaultTracker
         tracker.set(kGAIAppVersion, value: version)
+#endif
     }
 
     class func sendScreenName(name:String) {
+#if USE_GOOGLE_ANALYTICS
         let tracker = GAI.sharedInstance().defaultTracker
         tracker.send(GAIDictionaryBuilder.createAppView().set(name, forKey: kGAIScreenName).build())
+#endif
     }
 
     class func sendEvent(category:String?, action:String?, label:String? = nil, value:Int? = nil) {
+#if USE_GOOGLE_ANALYTICS
         let tracker = GAI.sharedInstance().defaultTracker
         tracker.send(GAIDictionaryBuilder.createEventWithCategory(category, action: action, label: label, value: value).build())
+#endif
     }
 }
